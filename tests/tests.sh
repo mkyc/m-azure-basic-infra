c0e626789908d6a88874bb90fdc1c9a7429f48ec
Ireneusz Glownia <ireneusz.glownia@pl.abb.com>
2020-10-02 20:15:36 +0200

    Fix mock path

--- tests/tests.sh
+++ tests/tests.sh
@@ -252,7 +252,7 @@ TESTS_DIR=${AZBI_K8S_VOL:-${TESTS_DIR_TMP}}
 MOUNT_DIR=${AZBI_MOUNT:-${TESTS_DIR_TMP}}
 
 # Create folder structure inside volume
-[ -z $AZBI_K8S_VOL ] > /dev/null || mkdir -p $AZBI_K8S_VOL/shared && cp -r mocks/ $AZBI_K8S_VOL 
+[ -z $AZBI_K8S_VOL ] > /dev/null || mkdir -p $AZBI_K8S_VOL/shared && cp -r tests/mocks/ $AZBI_K8S_VOL 
 
 # shellcheck disable=SC1090
 source "$(dirname "$0")/suite.sh"
