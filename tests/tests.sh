91a939ef2dccce701381b5325f801e4e17579f50
Ireneusz Glownia <ireneusz.glownia@pl.abb.com>
2020-10-02 22:12:13 +0200

    Fix local builds

--- tests/tests.sh
+++ tests/tests.sh
@@ -247,12 +247,16 @@ selfcheck
 # AZBI_K8S_VOL and AZBI_MOUNT are variables to set up when kubernetes based build agents are in use ('docker in docker')
 # AZBI_K8S_VOL - volume's mount point
 # AZBI_MOUNT - shared folder location on kubernetes host
+AZBI_K8S_VOL=${AZBI_K8S_VOL:-""}
+AZBI_MOUNT=${AZBI_MOUNT:=""}
 TESTS_DIR_TMP="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
 TESTS_DIR=${AZBI_K8S_VOL:-${TESTS_DIR_TMP}}
 MOUNT_DIR=${AZBI_MOUNT:-${TESTS_DIR_TMP}}
 
 # Create folder structure inside volume
-[ -z $AZBI_K8S_VOL ] > /dev/null || mkdir -p $AZBI_K8S_VOL/shared && cp -r tests/mocks/ $AZBI_K8S_VOL 
+if [[ $AZBI_K8S_VOL -ne "" ]]; then
+  mkdir -p $AZBI_K8S_VOL/shared && cp -r tests/mocks/ $AZBI_K8S_VOL
+fi
 
 # shellcheck disable=SC1090
 source "$(dirname "$0")/suite.sh"
