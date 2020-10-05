eeec23f6309f8a1f7e27869d0853f1a8ff0cbea1
Ireneusz Glownia <ireneusz.glownia@pl.abb.com>
2020-10-05 14:09:29 +0200

    Add variables quoting

--- tests/tests.sh
+++ tests/tests.sh
@@ -254,8 +254,8 @@ TESTS_DIR=${AZBI_K8S_VOL:=${TESTS_DIR_TMP}}
 MOUNT_DIR=${AZBI_MOUNT:=${TESTS_DIR_TMP}}
 
 # Create folder structure inside volume
-if [ $AZBI_K8S_VOL == "\/*" ]; then
-  mkdir -p $AZBI_K8S_VOL/shared && cp -r tests/mocks/ $AZBI_K8S_VOL
+if [ "$AZBI_K8S_VOL" == "\/*" ]; then
+  mkdir -p "$AZBI_K8S_VOL"/shared && cp -r "$TESTS_DIR_TMP"/tests/mocks/ "$AZBI_K8S_VOL"
 fi
 
 # shellcheck disable=SC1090
