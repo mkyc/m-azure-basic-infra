8bf9ebce7f36a2d2c017ce369c9aba32cdb33894
Mateusz Kyc <mateusz.kyc@gmail.com>
2020-10-16 14:41:17 +0200

    Add apply test logic

--- integration_test.go
+++ integration_test.go
@@ -2,11 +2,14 @@ package main
 
 import (
 	"bufio"
+	"context"
 	"crypto/rand"
 	"crypto/rsa"
 	"crypto/x509"
 	"encoding/pem"
 	"fmt"
+	"github.com/Azure/azure-sdk-for-go/profiles/2019-03-01/resources/mgmt/resources"
+	"github.com/Azure/go-autorest/autorest/azure/auth"
 	"io"
 	"io/ioutil"
 	"os"
@@ -27,7 +30,7 @@ const (
 func TestInit(t *testing.T) {
 	tests := []struct {
 		name               string
-		initParams         []string
+		initParams         map[string]string
 		wantOutput         string
 		wantConfigLocation string
 		wantConfigContent  string
@@ -62,8 +65,12 @@ azbi:
 `,
 		},
 		{
-			name:       "init 2 machines no public ips and named rg",
-			initParams: []string{"M_VMS_COUNT=2", "M_PUBLIC_IPS=false", "M_NAME=azbi-module-tests", "M_VMS_RSA=test_vms_rsa"},
+			name: "init 2 machines no public ips and named rg",
+			initParams: map[string]string{
+				"M_VMS_COUNT":  "2",
+				"M_PUBLIC_IPS": "false",
+				"M_NAME":       "azbi-module-tests",
+				"M_VMS_RSA":    "test_vms_rsa"},
 			wantOutput: `#AzBI | setup | ensure required directories
 #AzBI | ensure-state-file | checks if state file exists
 #AzBI | template-config-file | will template config file (and backup previous if exists)
@@ -94,13 +101,25 @@ azbi:
 
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			sharedPath, err := setup()
+			rsaName := "vms_rsa"
+			if v, ok := tt.initParams["M_VMS_RSA"]; ok {
+				rsaName = v
+			}
+			name := "epiphany"
+			if v, ok := tt.initParams["M_NAME"]; ok {
+				name = v
+			}
+			sharedPath, _, _, subscriptionId, _, err := setup(rsaName, name)
 			if err != nil {
 				t.Fatalf("setup() failed with: %v", err)
 			}
-			defer cleanup(sharedPath)
+			defer cleanup(sharedPath, subscriptionId, name)
 
-			initCommand := append([]string{"init"}, tt.initParams...)
+			initCommand := []string{"init"}
+			for k, v := range tt.initParams {
+				initCommand = append(initCommand, fmt.Sprintf("%s=%s", k, v))
+			}
+
 			initOpts := &docker.RunOptions{
 				Command: initCommand,
 				Remove:  true,
@@ -155,19 +174,19 @@ azbi:
 
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			sharedPath, err := setup()
-			if err != nil {
-				t.Fatalf("setup() failed with: %v", err)
-			}
 			rsaName := "vms_rsa"
 			if v, ok := tt.initParams["M_VMS_RSA"]; ok {
 				rsaName = v
 			}
-			err = generateRsaKeyPair(sharedPath, rsaName)
-			if err != nil {
-				t.Fatalf("wasnt able to create rsa file: %s", err)
+			name := "epiphany-rg"
+			if v, ok := tt.initParams["M_NAME"]; ok {
+				name = v
 			}
-			defer cleanup(sharedPath)
+			sharedPath, clientId, clientSecret, subscriptionId, tenantId, err := setup(rsaName, name)
+			if err != nil {
+				t.Fatalf("setup() failed with: %v", err)
+			}
+			defer cleanup(sharedPath, subscriptionId, name)
 
 			initCommand := []string{"init"}
 			for k, v := range tt.initParams {
@@ -182,28 +201,11 @@ azbi:
 			//in case of error Run function calls FailNow anyways
 			docker.Run(t, imageTag, initOpts)
 
-			armClientId := os.Getenv("ACI")
-			if len(armClientId) == 0 {
-				t.Fatalf("expected ACI environment variable with ARM_CLIENT_ID value")
-			}
-			armClientSecret := os.Getenv("ACS")
-			if len(armClientSecret) == 0 {
-				t.Fatalf("expected ACS environment variable with ARM_CLIENT_SECRET value")
-			}
-			armSubscriptionId := os.Getenv("ASI")
-			if len(armSubscriptionId) == 0 {
-				t.Fatalf("expected ASI environment variable with ARM_SUBSCRIPTION_ID value")
-			}
-			armTenantId := os.Getenv("ATI")
-			if len(armTenantId) == 0 {
-				t.Fatalf("expected ATI environment variable with ARM_TENANT_ID value")
-			}
-
 			planCommand := []string{"plan",
-				fmt.Sprintf("M_ARM_CLIENT_ID=%s", armClientId),
-				fmt.Sprintf("M_ARM_CLIENT_SECRET=%s", armClientSecret),
-				fmt.Sprintf("M_ARM_SUBSCRIPTION_ID=%s", armSubscriptionId),
-				fmt.Sprintf("M_ARM_TENANT_ID=%s", armTenantId),
+				fmt.Sprintf("M_ARM_CLIENT_ID=%s", clientId),
+				fmt.Sprintf("M_ARM_CLIENT_SECRET=%s", clientSecret),
+				fmt.Sprintf("M_ARM_SUBSCRIPTION_ID=%s", subscriptionId),
+				fmt.Sprintf("M_ARM_TENANT_ID=%s", tenantId),
 			}
 
 			planOpts := &docker.RunOptions{
@@ -242,17 +244,169 @@ azbi:
 	}
 }
 
-func setup() (string, error) {
-	wd, err := os.Getwd()
-	if err != nil {
-		return "", err
+func TestApply(t *testing.T) {
+	tests := []struct {
+		name                    string
+		initParams              map[string]string
+		wantPlanOutputLastLine  string
+		wantApplyOutputLastLine string
+	}{
+		{
+			name: "apply 2 machines no public ips and named rg",
+			initParams: map[string]string{
+				"M_VMS_COUNT":  "2",
+				"M_PUBLIC_IPS": "false",
+				"M_NAME":       "azbi-module-tests",
+				"M_VMS_RSA":    "test_vms_rsa"},
+			wantPlanOutputLastLine:  "Plan: 7 to add, 0 to change, 0 to destroy.",
+			wantApplyOutputLastLine: "#AzBI | terraform-output | will prepare terraform output",
+		},
 	}
-	p := path.Join(wd, "tests", "shared")
-	return p, os.MkdirAll(p, os.ModePerm)
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			rsaName := "vms_rsa"
+			if v, ok := tt.initParams["M_VMS_RSA"]; ok {
+				rsaName = v
+			}
+			name := "epiphany-rg"
+			if v, ok := tt.initParams["M_NAME"]; ok {
+				name = v
+			}
+			sharedPath, clientId, clientSecret, subscriptionId, tenantId, err := setup(rsaName, name)
+			if err != nil {
+				t.Fatalf("setup() failed with: %v", err)
+			}
+			defer cleanup(sharedPath, subscriptionId, name)
+
+			initCommand := []string{"init"}
+			for k, v := range tt.initParams {
+				initCommand = append(initCommand, fmt.Sprintf("%s=%s", k, v))
+			}
+			initOpts := &docker.RunOptions{
+				Command: initCommand,
+				Remove:  true,
+				Volumes: []string{fmt.Sprintf("%s:/shared", sharedPath)},
+			}
+
+			//in case of error Run function calls FailNow anyways
+			docker.Run(t, imageTag, initOpts)
+
+			planCommand := []string{"plan",
+				fmt.Sprintf("M_ARM_CLIENT_ID=%s", clientId),
+				fmt.Sprintf("M_ARM_CLIENT_SECRET=%s", clientSecret),
+				fmt.Sprintf("M_ARM_SUBSCRIPTION_ID=%s", subscriptionId),
+				fmt.Sprintf("M_ARM_TENANT_ID=%s", tenantId),
+			}
+
+			planOpts := &docker.RunOptions{
+				Command: planCommand,
+				Remove:  true,
+				Volumes: []string{fmt.Sprintf("%s:/shared", sharedPath)},
+			}
+
+			gotPlanOutput := docker.Run(t, imageTag, planOpts)
+			gotPlanOutputLastLine, err := getLastLineFromMultilineSting(gotPlanOutput)
+			if err != nil {
+				t.Fatalf("reading last line from multiline failed with: %v", err)
+			}
+
+			if diff := deep.Equal(gotPlanOutputLastLine, tt.wantPlanOutputLastLine); diff != nil {
+				t.Error(diff)
+			}
+
+			applyCommand := []string{"apply",
+				fmt.Sprintf("M_ARM_CLIENT_ID=%s", clientId),
+				fmt.Sprintf("M_ARM_CLIENT_SECRET=%s", clientSecret),
+				fmt.Sprintf("M_ARM_SUBSCRIPTION_ID=%s", subscriptionId),
+				fmt.Sprintf("M_ARM_TENANT_ID=%s", tenantId),
+			}
+
+			applyOpts := &docker.RunOptions{
+				Command: applyCommand,
+				Remove:  true,
+				Volumes: []string{fmt.Sprintf("%s:/shared", sharedPath)},
+			}
+
+			gotApplyOutput := docker.Run(t, imageTag, applyOpts)
+			gotApplyOutputLastLine, err := getLastLineFromMultilineSting(gotApplyOutput)
+			if err != nil {
+				t.Fatalf("reading last line from multiline failed with: %v", err)
+			}
+
+			if diff := deep.Equal(gotApplyOutputLastLine, tt.wantApplyOutputLastLine); diff != nil {
+				t.Error(diff)
+			}
+		})
+	}
 }
 
-func cleanup(sharedPath string) {
+func setup(rsaName string, rgName string) (sharedPath string, armClientId string, armClientSecret string, armSubscriptionId string, armTenantId string, err error) {
+	wd, err := os.Getwd()
+	if err != nil {
+		return
+	}
+	sharedPath = path.Join(wd, "tests", "shared")
+	err = os.MkdirAll(sharedPath, os.ModePerm)
+	if err != nil {
+		return
+	}
+	err = generateRsaKeyPair(sharedPath, rsaName)
+	if err != nil {
+		return
+	}
+	armClientId, armClientSecret, armSubscriptionId, armTenantId, err = loadEnvironments()
+	if err != nil {
+		return
+	}
+	if isResourceGroupPresent(armSubscriptionId, rgName) {
+		err = removeResourceGroup(armSubscriptionId, rgName)
+		if err != nil {
+			return
+		}
+	}
+	return
+}
+
+func removeResourceGroup(subscriptionId string, name string) error {
+	ctx := context.TODO()
+	groupsClient := resources.NewGroupsClient(subscriptionId)
+	authorizer, err := auth.NewAuthorizerFromEnvironment()
+	if err != nil {
+		fmt.Println("error")
+	}
+	groupsClient.Authorizer = authorizer
+	rgName := fmt.Sprintf("%s-rg", name)
+	gdf, err := groupsClient.Delete(ctx, rgName)
+	if err != nil {
+		return err
+	}
+	err = gdf.Future.WaitForCompletionRef(ctx, groupsClient.BaseClient.Client)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func isResourceGroupPresent(subscriptionId string, name string) bool {
+	groupsClient := resources.NewGroupsClient(subscriptionId)
+	authorizer, err := auth.NewAuthorizerFromEnvironment()
+	if err != nil {
+		fmt.Println("error")
+	}
+	groupsClient.Authorizer = authorizer
+	rgName := fmt.Sprintf("%s-rg", name)
+	_, err = groupsClient.Get(context.TODO(), rgName)
+	if err != nil {
+		return false
+	} else {
+		return true
+	}
+}
+
+func cleanup(sharedPath string, subscriptionId string, name string) {
 	_ = os.RemoveAll(sharedPath)
+	_ = removeResourceGroup(subscriptionId, name)
 }
 
 func generateRsaKeyPair(directory string, name string) error {
@@ -288,4 +442,28 @@ func getLastLineFromMultilineSting(s string) (string, error) {
 			return string(line), nil
 		}
 	}
+}
+
+func loadEnvironments() (armClientId string, armClientSecret string, armSubscriptionId string, armTenantId string, err error) {
+	armClientId = os.Getenv("AZURE_CLIENT_ID")
+	if len(armClientId) == 0 {
+		err = fmt.Errorf("expected AZURE_CLIENT_ID environment variable")
+		return
+	}
+	armClientSecret = os.Getenv("AZURE_CLIENT_SECRET")
+	if len(armClientSecret) == 0 {
+		err = fmt.Errorf("expected AZURE_CLIENT_SECRET environment variable")
+		return
+	}
+	armSubscriptionId = os.Getenv("AZURE_SUBSCRIPTION_ID")
+	if len(armSubscriptionId) == 0 {
+		err = fmt.Errorf("expected AZURE_SUBSCRIPTION_ID environment variable")
+		return
+	}
+	armTenantId = os.Getenv("AZURE_TENANT_ID")
+	if len(armTenantId) == 0 {
+		err = fmt.Errorf("expected AZURE_TENANT_ID environment variable")
+		return
+	}
+	return
 }
