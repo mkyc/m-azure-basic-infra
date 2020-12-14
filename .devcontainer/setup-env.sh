80a8f5f7e9ae3ec9357cf08fb169f558f3f1c654
Mateusz Kyc <mateusz.kyc@gmail.com>
2020-12-14 14:50:55 +0100

    .devcontainer dir fixed

--- .devcontainer/setup-env.sh
+++ .devcontainer/setup-env.sh
@@ -1,6 +1,5 @@
 git config --global http.sslCAinfo /etc/ssl/certs/ca-certificates.crt
 echo 'export M_VERSION="dev"' >> ~/.bashrc
-echo 'export M_WORKDIR="$(pwd)/workdir"' >> ~/.bashrc
 echo 'export M_RESOURCES="$(pwd)/resources"' >> ~/.bashrc
 echo 'export M_SHARED="$(pwd)/shared"' >> ~/.bashrc
 cd $(pwd)/resources/terraform && terraform init
