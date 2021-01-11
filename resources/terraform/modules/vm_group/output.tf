ed24108350221d6d86605fd8d98fcf19733c95b7
atsikham <atsikham@users.noreply.github.com>
2021-01-11 16:29:47 +0500

    Yet another output format change

--- resources/terraform/modules/vm_group/output.tf
+++ resources/terraform/modules/vm_group/output.tf
@@ -1,10 +1,13 @@
 output "vm_group" {
-  value = [
+  value = {
+    vm_group: var.vm_group.name,
+    vms: [
     for vm in azurerm_linux_virtual_machine.vm:
     {
       vm_name: vm.name
       public_ip: vm.public_ip_address
       private_ips: vm.private_ip_addresses
     }
-  ]
+    ]
+  }
 }
